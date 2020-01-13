8de15dd997ef5e2ba8ddd86ce23671d3ff34635f
Hiromitsu Yamaguchi <yamagame@mac.com>
2020-01-13 09:37:24 +0900

    サーバー側サンプルコード追加

--- server.js
+++ server.js
@@ -5,33 +5,46 @@ const app = express();
 
 const PORT = process.env.PORT || 3090;
 let sockets = [];
+let barData = {};
+let calendarData = {};
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ type: 'application/json' }))
 
 app.use((req, res, next) => {
   console.log(`${req.method}:${req.url}`);
+  console.log(`${JSON.stringify(req.body,null,'  ')}`);
   next();
 })
 
 app.post('/bar/all', (req, res) => {
-  res.sendStatus(200);
+  res.json(Object.keys(barData).map( k => barData[k] ));
 })
 
 app.post('/bar/update', (req, res) => {
+  req.body.barData.forEach( bar => {
+    if (typeof(barData[bar.uuid]) === 'undefined') barData[bar.uuid] = {};
+    Object.keys(bar).forEach( k => {
+      barData[bar.uuid][k] = bar[k];
+    })
+  })
   res.sendStatus(200);
 })
 
 app.post('/bar/delete', (req, res) => {
+  req.body.barData.forEach( bar => {
+    delete barData[bar.uuid];
+  })
   res.sendStatus(200);
 })
 
 app.post('/calendar', (req, res) => {
+  calendarData = req.body.calendarData;
   res.sendStatus(200);
 })
 
 app.get('/calendar', (req, res) => {
-  res.sendStatus(200);
+  res.json(calendarData);
 })
 
 const server = require('http').Server(app);
